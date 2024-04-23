a130b384e18e5ee9210544c991464829e11d801a
Felipe Bravim <felipe.bravim@gmail.com>
2024-04-23 18:15:02 -0300

    Classe Usuarios e métodos. Finalização do projeto.

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,24 @@
+import org.w3c.dom.ls.LSOutput;
+
 import java.util.Scanner;
 
 public class Main {
+
+    static Usuario usuario = new Usuario();
+    static Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
 
         //Variáveis
         int opcao = 0;
-        Scanner scanner = new Scanner(System.in);
         String view = """
                     ********************
                     Usuário: %s
-                    Conta: %i
+                    Conta: %s
                     Saldo: %.2f
                     ********************
-                    
-                    """;
+                    """.formatted(usuario.nome, usuario.conta, usuario.valor);
+
         String menu = """
                 Operações
                 
@@ -25,32 +30,54 @@ public class Main {
                 Digite a opção desejada:
                 """;
 
-        //Métodos
-
-
         //Código
+        System.out.println(view);
+
         while(opcao != 4){
-            System.out.println(view);
             System.out.println(menu);
             opcao = scanner.nextInt();
 
             switch (opcao){
                 case 1:
-
+                    consultarSaldo();
                 break;
                 case 2:
-
+                    receberValor();
                 break;
                 case 3:
-
+                    transferirValor();
                 break;
                 case 4:
 
                 break;
                 default:
-
+                    System.out.println("Favor escolher dentro de uma das opções acima.");
                 break;
             }
+        }
+    }
+
+    //Métodos
+    private static void consultarSaldo() {
+        System.out.println("Seu saldo é de: " + usuario.valor);
+    }
+
+    private static void receberValor() {
+        System.out.println("Informe o valor a receber:");
+        double valor = scanner.nextInt();
+        usuario.valor += valor;
+        System.out.println("Saldo atualizado: " + usuario.valor);
+    }
+
+    private static void transferirValor(){
+        System.out.println("Informe valor a ser transferido:");
+        double valor = scanner.nextInt();
+
+        if(valor > usuario.valor){
+            System.out.println("Não é possível transferir um valor maior que o saldo da conta");
+        } else {
+            usuario.valor -= valor;
+            System.out.println("Saldo atualizado: " + usuario.valor);
         }
     }
 }
---
+++ src/Usuario.java
@@ -0,0 +1,5 @@
+public class Usuario {
+        String nome = "Felipe";
+        String conta = "Corrente";
+        double valor = 2500.00;
+}
